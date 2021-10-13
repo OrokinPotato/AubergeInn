4f5e7da9a78354163977525fa021c048bc1ad240
Camille <camille.a.gauthier@usherbrooke.ca>
2021-10-12 22:23:14 -0400

    Ajouté preparedStatements et méthodes à tableHandlerClients

--- src/AubergeInn/Table/tableHandlerClients.java
+++ src/AubergeInn/Table/tableHandlerClients.java
@@ -7,10 +7,55 @@ import java.sql.*;
 public class tableHandlerClients {
 
     private PreparedStatement stmtExiste;
+    private PreparedStatement stmtAfficher;
     private PreparedStatement stmtInsert;
-    private PreparedStatement stmtUpdateIncrNbPret;
-    private PreparedStatement stmtUpdateDecNbPret;
     private PreparedStatement stmtDelete;
     private Connexion cx;
 
+    /**
+     * Creation d'une instance. Précompilation d'énoncés SQL.
+     */
+    public tableHandlerClients(Connexion cx) throws SQLException
+    {
+        this.cx = cx;
+        stmtExiste = cx.getConnection()
+                .prepareStatement("select id, prenom, nom, age from Clients where id = ?");
+        stmtAfficher = cx.getConnection()
+                .prepareStatement("select id, prenom, nom, age from Clients");
+        stmtInsert = cx.getConnection().prepareStatement(
+                "insert into Clients (id, prenom, nom, age) " + "values (?,?,?,?,?)");
+        stmtDelete = cx.getConnection().prepareStatement("delete from Clients where id = ?");
+    }
+
+
+    /**
+     * Ajout d'un nouveau client.
+     */
+    public void ajouterClient(int idClient, String prenom, String nom, int age) throws SQLException
+    {
+        /* Ajout du client. */
+        stmtInsert.setInt(1, idClient);
+        stmtInsert.setString(2, prenom);
+        stmtInsert.setString(3, nom);
+        stmtInsert.setInt(4, age);
+        stmtInsert.executeUpdate();
+    }
+
+    /**
+     * Suppression d'un client.
+     */
+    public void supprimerClient(int idClient) throws SQLException
+    {
+        stmtDelete.setInt(1, idClient);
+        stmtDelete.executeUpdate();
+    }
+
+    /**
+     * Affichage de toutes les informations sur un client.
+     */
+    public void afficherClient(int idClient) throws SQLException
+    {
+        /* TO DO */
+        // Je me demande si ça va vraiment ici ou dans le handler de Reservations
+    }
 }
